edbde0ac5e4685b75104aba068bd0223c4e90bef
Louis Dyer <louisalexanderdyer@gmail.com>
2023-07-20 20:35:25 +1200

    Moved font location to attempt deployment

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -1,10 +1,13 @@
-import { fingerPaint } from '../layout'
+// import { fingerPaint } from '../layout'
+import { Finger_Paint } from 'next/font/google'
+
+const fingerPaint = Finger_Paint({ subsets: ['latin'], weight: ['400'] })
 
 export function Title() {
   return (
     <div className="flex flex-col items-center">
       <h1
-        className={`${fingerPaint.className} text-7xl md:text-8xl xl:text-9xl`}
+        className={`${fingerPaint.className} text-5xl md:text-6xl xl:text-7xl 2xl:text-8xl`}
       >
         Swing
       </h1>
