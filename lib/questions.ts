cae2ff05d28f327fda330bab3cd908f478f737ff
Louis Dyer <louisalexanderdyer@gmail.com>
2023-07-25 14:50:50 +1200

    add fake data

--- lib/questions.ts
+++ lib/questions.ts
@@ -2,27 +2,300 @@ const dataObj = {
   questions: [
     {
       id: 1,
-      question: 'How are you?',
-      options: { posi: 'good', neut: 'okay', negi: 'bad' },
-    },
-    {
-      id: 1,
-      question: 'Rate my sexy body?',
-      options: { posi: '5', negi: '1' },
-    },
-    {
-      id: 1,
-      question: 'How are you?',
+      question: 'whats the definition of could?',
       options: {
-        posi: 'I would like know more',
-        neut: "I don't see colour",
-        negi: 'Fuck te reo signs',
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
       },
-      reoQuestion: 'Kei te pia koe?',
+      reoQuestion: 'whats the definition of rope?',
       reoOptions: {
-        posi: 'ka pai',
-        neut: 'ae',
-        negi: 'ai',
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+    },
+    {
+      id: 2,
+      question: 'whats the definition of cotton?',
+      options: {
+        posi: '1',
+        neut: '3',
+        negi: '5',
+      },
+      reoQuestion: 'whats the definition of frozen?',
+      reoOptions: {
+        posi: '1',
+        neut: '3',
+        negi: '5',
+      },
+    },
+    {
+      id: 3,
+      question: 'whats the definition of six?',
+      options: {
+        posi: 'public sector',
+        negi: 'private sector',
+      },
+      reoQuestion: 'whats the definition of knew?',
+      reoOptions: {
+        posi: 'public sector',
+        negi: 'private sector',
+      },
+    },
+    {
+      id: 4,
+      question: 'whats the definition of practical?',
+      options: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+      reoQuestion: 'whats the definition of broken?',
+      reoOptions: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+    },
+    {
+      id: 5,
+      question: 'whats the definition of shelf?',
+      options: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+      reoQuestion: 'whats the definition of tune?',
+      reoOptions: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+    },
+    {
+      id: 6,
+      question: 'whats the definition of heat?',
+      options: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+      reoQuestion: 'whats the definition of completely?',
+      reoOptions: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+    },
+    {
+      id: 7,
+      question: 'whats the definition of carbon?',
+      options: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+      reoQuestion: 'whats the definition of pass?',
+      reoOptions: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+    },
+    {
+      id: 8,
+      question: 'whats the definition of birthday?',
+      options: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+      reoQuestion: 'whats the definition of direct?',
+      reoOptions: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+    },
+    {
+      id: 9,
+      question: 'whats the definition of hat?',
+      options: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+      reoQuestion: 'whats the definition of while?',
+      reoOptions: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+    },
+    {
+      id: 10,
+      question: 'whats the definition of many?',
+      options: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+      reoQuestion: 'whats the definition of rule?',
+      reoOptions: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+    },
+    {
+      id: 11,
+      question: 'whats the definition of border?',
+      options: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+      reoQuestion: 'whats the definition of bite?',
+      reoOptions: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+    },
+    {
+      id: 12,
+      question: 'whats the definition of throw?',
+      options: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+      reoQuestion: 'whats the definition of great?',
+      reoOptions: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+    },
+    {
+      id: 13,
+      question: 'whats the definition of garage?',
+      options: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+      reoQuestion: 'whats the definition of expression?',
+      reoOptions: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+    },
+    {
+      id: 14,
+      question: 'whats the definition of goose?',
+      options: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+      reoQuestion: 'whats the definition of fort?',
+      reoOptions: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+    },
+    {
+      id: 15,
+      question: 'whats the definition of tightly?',
+      options: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+      reoQuestion: 'whats the definition of skin?',
+      reoOptions: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+    },
+    {
+      id: 16,
+      question: 'whats the definition of church?',
+      options: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+      reoQuestion: 'whats the definition of real?',
+      reoOptions: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+    },
+    {
+      id: 17,
+      question: 'whats the definition of highway?',
+      options: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+      reoQuestion: 'whats the definition of tiny?',
+      reoOptions: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+    },
+    {
+      id: 18,
+      question: 'whats the definition of herd?',
+      options: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+      reoQuestion: 'whats the definition of view?',
+      reoOptions: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+    },
+    {
+      id: 19,
+      question: 'whats the definition of practice?',
+      options: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+      reoQuestion: 'whats the definition of forty?',
+      reoOptions: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+    },
+    {
+      id: 20,
+      question: 'whats the definition of volume?',
+      options: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
+      },
+      reoQuestion: 'whats the definition of away?',
+      reoOptions: {
+        posi: 'yes',
+        neut: 'meh',
+        negi: 'nah',
       },
     },
   ],
